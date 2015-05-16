fbbb13a7a956a0956c0318326d938d2daf5d9d46
anonymous <anonymous@hannover.freifunk.net>
2015-05-16 03:32:23 +0200

    assorted changes
    -> http://whatthecommit.com

--- site.mk
+++ site.mk
@@ -18,13 +18,14 @@ GLUON_SITE_PACKAGES := \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
+	gluon-setup-mode \
 	gluon-status-page \
 	iwinfo \
 	iptables \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.10e-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.10f-$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
