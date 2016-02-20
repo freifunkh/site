5c73c42ccd3f6744c4837a90017a861b75ce8e54
anonymous <anonymous@hannover.freifunk.net>
2016-02-20 19:08:52 +0100

    Removed code.
    -> http://whatthecommit.com

--- site.mk
+++ site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
 	iwinfo
 
 
-DEFAULT_GLUON_RELEASE := 0.12a-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.12b-$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
