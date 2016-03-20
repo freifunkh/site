6b7d17a3c3fd8783e215d3550ac7799f105d7bf0
anonymous <anonymous@hannover.freifunk.net>
2016-03-20 02:22:18 +0100

    This bunny should be killed.
    -> http://whatthecommit.com

--- site.mk
+++ site.mk
@@ -1,5 +1,6 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
+	gluon-respondd \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
@@ -17,7 +18,6 @@ GLUON_SITE_PACKAGES := \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
-	gluon-respondd \
 	gluon-setup-mode \
 	gluon-status-page \
 	haveged \
