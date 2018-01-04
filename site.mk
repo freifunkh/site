9ba915e11d7e7c3ca447125486c2cafc680029bb
lemoer <git@irrelefant.net>
2018-01-04 16:24:45 +0100

    site.mk: add gluon-ebtables-segment-mld packet (#13)

--- site.mk
+++ site.mk
@@ -9,6 +9,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-ebtables-segment-mld \
 	gluon-web-admin \
 	gluon-web-network \
 	gluon-web-private-wifi \
