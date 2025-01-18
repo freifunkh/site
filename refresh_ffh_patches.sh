ddf12c62206eb7eeeca830ebaf6847bb4f3513a0
aiyion.prime <git@aiyionpri.me>
2025-01-18 17:37:47 +0100

    patches: refresh upon Gluon v2023.2.4

--- refresh_ffh_patches.sh
+++ refresh_ffh_patches.sh
@@ -13,7 +13,7 @@ git clone https://github.com/freifunk-gluon/gluon.git $PATCHDIR
 
 cd $PATCHDIR
 
-git checkout v2022.1.4
+git checkout v2023.2.4
 
 # apply all site patches
 for patch in "../patches"/*.patch; do
@@ -24,7 +24,7 @@ git checkout -B patched >/dev/null
 
 # recreate site patches from just created commits
 n=0
-for commit in $(git rev-list --reverse --no-merges v2022.1.4..patched); do
+for commit in $(git rev-list --reverse --no-merges v2023.2.4..patched); do
 	(( ++n ))
 	echo "Updating: $(git log --format=%s -n 1 "$commit")"
 	git -c core.abbrev=40 show --pretty=format:'From: %an <%ae>%nDate: %aD%nSubject: %B' --no-renames --binary "$commit" > "../patches/$(printf '%04u' "$n")-$(git show -s --pretty=format:%f "$commit").patch"
